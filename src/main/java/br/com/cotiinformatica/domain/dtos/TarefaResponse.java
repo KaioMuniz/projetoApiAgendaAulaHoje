c6e3b08ca40bb60b3ebaaeddc3348fa69c98c7d2
KaioMuniz <kkaioribeiro@gmail.com>
2025-06-28 11:24:58 -0300

    tarefaresponse

--- src/main/java/br/com/cotiinformatica/domain/dtos/TarefaResponse.java
+++ src/main/java/br/com/cotiinformatica/domain/dtos/TarefaResponse.java
@@ -1,5 +1,23 @@
 package br.com.cotiinformatica.domain.dtos;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
 public class TarefaResponse {
 
+	private UUID id;
+	private String nome;
+	private String descricao;
+	private boolean concluida;
+	
+	@JsonFormat(
+			shape = JsonFormat.Shape.STRING,
+			pattern = "yyyy-MM-dd'T'HH:mm"
+		)
+	private LocalDateTime dataHora;	
 }
